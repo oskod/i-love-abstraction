caa3a06d8970aa6f06a8399306753e16ee38e2d5
dokso <125161302+oskod@users.noreply.github.com>
2023-04-16 01:37:47 +0200

    Create web.js
    awesome

---
+++ web.js
@@ -0,0 +1,106 @@
+const http = require("node:http");
+const https = require("node:https");
+
+class Route {
+	routes = {};
+	handlers = {};
+
+	addMethod(method, handler) {
+		if (this.handlers[method]) {
+			return console.error(`Method ${method} is already occupied.`);
+		}
+		this.handlers[method] = handler;
+
+		return this;
+	}
+
+	addRoute(routePath, route) {
+		if (routePath.length > 1) {
+			let foundRoute = this.routes[routePath[0]];
+			if (!foundRoute) {
+				foundRoute = new Route();
+				this.routes[routePath[0]] = foundRoute;
+			}
+			foundRoute.addRoute(routePath.splice(1), route);
+		} else {
+			this.routes[routePath[0]] = route;
+		}
+
+		return this;
+	}
+
+	handleRequest(routePath, req, res) {
+		const method = req.method;
+		const foundStarter = this.routes["*"];
+
+		if (foundStarter && foundStarter.handleRequest([], req, res) !== true) {
+			return;
+		}
+
+		if (routePath.length > 0) {
+			const foundRoute = this.routes[routePath[0]];
+			if (!foundRoute) {
+				const foundEnder = this.routes["**"];
+				if (foundEnder) {
+					return foundEnder.handleRequest([], req, res);
+				}
+
+				res.writeHead(404);
+				res.end("Not found");
+
+				return;
+			}
+
+			return foundRoute.handleRequest(routePath.splice(1), req, res);
+		}
+
+		const foundHandler = this.handlers[method];
+
+		if (!foundHandler) {
+			res.writeHead(404);
+			res.end("Not found");
+
+			return;
+		} else {
+			return foundHandler(req, res);
+		}
+	}
+}
+
+class WebServer {
+	app = null;
+
+	constructor(isHttps) {
+		const currentServer = this;
+		this.app = (isHttps ? https : http).createServer(function(req, res) {
+			currentServer.handleRequest(req, res);
+		});
+		this.rootRoute = new Route();
+		this.rootRoute.root = true;
+	}
+
+	listen(...args) {
+		return this.app.listen(...args)
+	}
+
+	addRoute(routeStr) {
+		if (routeStr[0] === "/") {
+			routeStr = routeStr.substring(1);
+		}
+
+		const routePath = routeStr.split("/");
+
+		const route = new Route();
+		this.rootRoute.addRoute(routePath, route);
+
+		return route;
+	}
+
+	handleRequest(req, res) {
+		const path = req.url.slice(1).split("?")[0].split("/");
+
+		this.rootRoute.handleRequest(path, req, res);
+	}
+}
+
+module.exports = {WebServer, Route};
